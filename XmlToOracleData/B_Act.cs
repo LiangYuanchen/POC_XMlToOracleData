b9b3a9da6aaaa0dd1035b6f8fe4fdfeef64d7263
LiangYuanchen <liangyuanchen@163.com>
2013-08-22 11:45:23 +0800

    更改1
    更改1

---
+++ XmlToOracleData/B_Act.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.IO;
+using System.Xml.Serialization;
+namespace XmlToOracleData
+{
+    public class SerializationHelper
+    {
+        /// <summary>
+        /// Serialize the object into an XML format
+        /// </summary>
+        /// <typeparam name="T">Type of object to serialize</typeparam>
+        /// <param name="pObject">the object to serialize</param>
+        /// <returns>a string representing the XML version of the object</returns>
+        public static String SerializeObject<T>(T pObject)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            UTF8Encoding encoding = new UTF8Encoding();
+
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            System.Xml.XmlTextWriter xmlTextWriter = new System.Xml.XmlTextWriter(memoryStream, Encoding.UTF8);
+            xs.Serialize(xmlTextWriter, (object)pObject);
+            memoryStream = (MemoryStream)xmlTextWriter.BaseStream;
+            return encoding.GetString(memoryStream.ToArray());
+        }
+
+        /// <summary>
+        /// Deserialize the object back into the object from an XML string
+        /// </summary>
+        /// <typeparam name="T">Type of the object to restore</typeparam>
+        /// <param name="pXmlizedString">The string that represents the object in XML</param>
+        /// <returns>A new instance of the restored object</returns>
+        public static T DeserializeObject<T>(String pXmlizedString)
+        {
+            UTF8Encoding encoding = new UTF8Encoding();
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            MemoryStream memoryStream = new MemoryStream(encoding.GetBytes(pXmlizedString));
+            System.Xml.XmlTextWriter xmlTextWriter = new System.Xml.XmlTextWriter(memoryStream, Encoding.UTF8);
+            return (T)xs.Deserialize(memoryStream);
+        }
+    }
+
+    public class root
+    {
+        public act act { get; set; }
+        public root()
+        {
+            act = new act();
+        }
+    }
+  
+   public  class act
+    {
+       public act()
+       {
+           actitems = new List<item>();
+           actcorrelations = new List<actcorrelation>();
+       }
+        [XmlAttribute]
+        public string actid { get; set; }
+        public string actname { get; set; }
+        public string fileNumber { get; set; }
+        public string pubdate { get; set; }
+        public string stadate { get; set; }
+        public string depts { get; set; }
+        public string deptids { get; set; }
+        public string state { get; set; }
+        public string opdate { get; set; }
+        public string effect { get; set; }
+        public string content { get; set; }
+        public string enddate { get; set; }
+        public List<item> actitems { get; set; }
+        public List<actcorrelation> actcorrelations { get; set; }
+    }
+   public class item
+    {
+       [XmlAttribute]
+        public string itemid { get; set; }
+        public string actid { get; set; }
+        public string itemname { get; set; }
+        public string itemtype { get; set; }
+        public string orders { get; set; }
+        public string content { get; set; }
+        public string itemparentid { get; set; }
+    }
+   public class actcorrelation
+    {
+        public string toactid { get; set; }
+        public string toitemid { get; set; }
+        public string fromactid { get; set; }
+        public string fromactname { get; set; }
+        public string fromitemid { get; set; }
+        public string fromitemcontent { get; set; }
+        public string fromitemname { get; set; }
+    }
+}
---
+++ XmlToOracleData/T_CP_ACT.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Data;
+
+namespace XmlToOracleData
+{
+    public partial class T_CP_ACT
+        {
+            /// <summary>
+            /// ACTID
+            /// </summary>		
+            private decimal _actid;
+            public decimal ACTID
+            {
+                get { return _actid; }
+                set { _actid = value; }
+            }
+            /// <summary>
+            /// ACTNAME
+            /// </summary>		
+            private string _actname;
+            public string ACTNAME
+            {
+                get { return _actname; }
+                set { _actname = value; }
+            }
+            /// <summary>
+            /// FILENUMBER
+            /// </summary>		
+            private string _filenumber;
+            public string FILENUMBER
+            {
+                get { return _filenumber; }
+                set { _filenumber = value; }
+            }
+            /// <summary>
+            /// PUBDATE
+            /// </summary>		
+            private DateTime? _pubdate;
+            public DateTime? PUBDATE
+            {
+                get { return _pubdate; }
+                set { _pubdate = value; }
+            }
+            /// <summary>
+            /// STADATE
+            /// </summary>		
+            private DateTime? _stadate;
+            public DateTime? STADATE
+            {
+                get { return _stadate; }
+                set { _stadate = value; }
+            }
+            /// <summary>
+            /// DEPTS
+            /// </summary>		
+            private string _depts;
+            public string DEPTS
+            {
+                get { return _depts; }
+                set { _depts = value; }
+            }
+            /// <summary>
+            /// CONTENT
+            /// </summary>		
+            private string _content;
+            public string CONTENT
+            {
+                get { return _content; }
+                set { _content = value; }
+            }
+            /// <summary>
+            /// CREATEDTIME
+            /// </summary>		
+            private DateTime _createdtime;
+            public DateTime CREATEDTIME
+            {
+                get { return _createdtime; }
+                set { _createdtime = value; }
+            }
+            /// <summary>
+            /// EFFECT
+            /// </summary>		
+            private string _effect;
+            public string EFFECT
+            {
+                get { return _effect; }
+                set { _effect = value; }
+            }
+            /// <summary>
+            /// ID
+            /// </summary>		
+            private decimal _id;
+            public decimal ID
+            {
+                get { return _id; }
+                set { _id = value; }
+            }
+            /// <summary>
+            /// ACTCODE
+            /// </summary>		
+            private string _actcode;
+            public string ACTCODE
+            {
+                get { return _actcode; }
+                set { _actcode = value; }
+            }
+            /// <summary>
+            /// DEPTIDS
+            /// </summary>		
+            private string _deptids;
+            public string DEPTIDS
+            {
+                get { return _deptids; }
+                set { _deptids = value; }
+            }
+            /// <summary>
+            /// SRCTYPE
+            /// </summary>		
+            private string _srctype;
+            public string SRCTYPE
+            {
+                get { return _srctype; }
+                set { _srctype = value; }
+            }
+            /// <summary>
+            /// STATUS
+            /// </summary>		
+            private string _status;
+            public string STATUS
+            {
+                get { return _status; }
+                set { _status = value; }
+            }
+            /// <summary>
+            /// DELFLAG
+            /// </summary>		
+            private string _delflag;
+            public string DELFLAG
+            {
+                get { return _delflag; }
+                set { _delflag = value; }
+            }
+            /// <summary>
+            /// CREATOR
+            /// </summary>		
+            private string _creator;
+            public string CREATOR
+            {
+                get { return _creator; }
+                set { _creator = value; }
+            }
+            /// <summary>
+            /// UPDATOR
+            /// </summary>		
+            private string _updator;
+            public string UPDATOR
+            {
+                get { return _updator; }
+                set { _updator = value; }
+            }
+            /// <summary>
+            /// UPDATETIME
+            /// </summary>		
+            private DateTime _updatetime;
+            public DateTime UPDATETIME
+            {
+                get { return _updatetime; }
+                set { _updatetime = value; }
+            }
+            /// <summary>
+            /// ENDDATE
+            /// </summary>		
+            private DateTime? _enddate;
+            public DateTime? ENDDATE
+            {
+                get { return _enddate; }
+                set { _enddate = value; }
+            }
+
+    }
+
+
+}
