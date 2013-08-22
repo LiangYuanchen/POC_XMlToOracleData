b13d20d66dc4103a734e634b53dede5b1e8660a7
LiangYuanchen <liangyuanchen@163.com>
2013-08-22 11:41:54 +0800

    首次
    首次

---
+++ DAL.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Text;
+using System.Data.OracleClient;
+using System.Collections.Generic;
+using System.Data;
+namespace XmlToOracleData
+{
+        public partial class T_CP_ACT
+        {
+
+            public bool Exists(decimal ACTID, string ACTNAME, string FILENUMBER, DateTime PUBDATE, DateTime STADATE, string DEPTS, string CONTENT, DateTime CREATEDTIME, string EFFECT, decimal ID, string ACTCODE, string DEPTIDS, string SRCTYPE, string STATUS, string DELFLAG, string CREATOR, string UPDATOR, DateTime UPDATETIME, DateTime ENDDATE)
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.Append("select count(1) from T_CP_ACT");
+                strSql.Append(" where ");
+                strSql.Append(" ACTID = :ACTID and  ");
+                strSql.Append(" ACTNAME = :ACTNAME and  ");
+                strSql.Append(" FILENUMBER = :FILENUMBER and  ");
+                strSql.Append(" PUBDATE = :PUBDATE and  ");
+                strSql.Append(" STADATE = :STADATE and  ");
+                strSql.Append(" DEPTS = :DEPTS and  ");
+                strSql.Append(" CONTENT = :CONTENT and  ");
+                strSql.Append(" CREATEDTIME = :CREATEDTIME and  ");
+                strSql.Append(" EFFECT = :EFFECT and  ");
+                strSql.Append(" ID = :ID and  ");
+                strSql.Append(" ACTCODE = :ACTCODE and  ");
+                strSql.Append(" DEPTIDS = :DEPTIDS and  ");
+                strSql.Append(" SRCTYPE = :SRCTYPE and  ");
+                strSql.Append(" STATUS = :STATUS and  ");
+                strSql.Append(" DELFLAG = :DELFLAG and  ");
+                strSql.Append(" CREATOR = :CREATOR and  ");
+                strSql.Append(" UPDATOR = :UPDATOR and  ");
+                strSql.Append(" UPDATETIME = :UPDATETIME and  ");
+                strSql.Append(" ENDDATE = :ENDDATE  ");
+                OracleParameter[] parameters = {
+					new OracleParameter(":ACTID", OracleType.Number,32),
+					new OracleParameter(":ACTNAME", OracleType.VarChar,1024),
+					new OracleParameter(":FILENUMBER", OracleType.VarChar,4000),
+					new OracleParameter(":PUBDATE", OracleType.DateTime),
+					new OracleParameter(":STADATE", OracleType.DateTime),
+					new OracleParameter(":DEPTS", OracleType.VarChar,1024),
+					new OracleParameter(":CONTENT", OracleType.Clob,4000),
+					new OracleParameter(":CREATEDTIME", OracleType.DateTime),
+					new OracleParameter(":EFFECT", OracleType.Char,2),
+					new OracleParameter(":ID", OracleType.Number,32),
+					new OracleParameter(":ACTCODE", OracleType.VarChar,64),
+					new OracleParameter(":DEPTIDS", OracleType.VarChar,640),
+					new OracleParameter(":SRCTYPE", OracleType.Char,2),
+					new OracleParameter(":STATUS", OracleType.Char,2),
+					new OracleParameter(":DELFLAG", OracleType.Char,2),
+					new OracleParameter(":CREATOR", OracleType.VarChar,64),
+					new OracleParameter(":UPDATOR", OracleType.VarChar,64),
+					new OracleParameter(":UPDATETIME", OracleType.DateTime),
+					new OracleParameter(":ENDDATE", OracleType.DateTime)			};
+                parameters[0].Value = ACTID;
+                parameters[1].Value = ACTNAME;
+                parameters[2].Value = FILENUMBER;
+                parameters[3].Value = PUBDATE;
+                parameters[4].Value = STADATE;
+                parameters[5].Value = DEPTS;
+                parameters[6].Value = CONTENT;
+                parameters[7].Value = CREATEDTIME;
+                parameters[8].Value = EFFECT;
+                parameters[9].Value = ID;
+                parameters[10].Value = ACTCODE;
+                parameters[11].Value = DEPTIDS;
+                parameters[12].Value = SRCTYPE;
+                parameters[13].Value = STATUS;
+                parameters[14].Value = DELFLAG;
+                parameters[15].Value = CREATOR;
+                parameters[16].Value = UPDATOR;
+                parameters[17].Value = UPDATETIME;
+                parameters[18].Value = ENDDATE;
+
+                return DbHelperOra.Exists(strSql.ToString(), parameters);
+            }
+
+
+
+            /// <summary>
+            /// 增加一条数据
+            /// </summary>
+            public void Add(T_CP_ACT model)
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.Append("insert into T_CP_ACT(");
+                strSql.Append("ACTID,ID,ACTCODE,DEPTIDS,SRCTYPE,STATUS,DELFLAG,CREATOR,UPDATOR,UPDATETIME,ENDDATE,ACTNAME,FILENUMBER,PUBDATE,STADATE,DEPTS,CONTENT,CREATEDTIME,EFFECT");
+                strSql.Append(") values (");
+                strSql.Append(":ACTID,:ID,:ACTCODE,:DEPTIDS,:SRCTYPE,:STATUS,:DELFLAG,:CREATOR,:UPDATOR,:UPDATETIME,:ENDDATE,:ACTNAME,:FILENUMBER,:PUBDATE,:STADATE,:DEPTS,:CONTENT,:CREATEDTIME,:EFFECT");
+                strSql.Append(") ");
+
+                OracleParameter[] parameters = {
+			            new OracleParameter(":ACTID", OracleType.Number,32) ,            
+                        new OracleParameter(":ID", OracleType.Number,32) ,            
+                        new OracleParameter(":ACTCODE", OracleType.VarChar,64) ,            
+                        new OracleParameter(":DEPTIDS", OracleType.VarChar,640) ,            
+                        new OracleParameter(":SRCTYPE", OracleType.Char,2) ,            
+                        new OracleParameter(":STATUS", OracleType.Char,2) ,            
+                        new OracleParameter(":DELFLAG", OracleType.Char,2) ,            
+                        new OracleParameter(":CREATOR", OracleType.VarChar,64) ,            
+                        new OracleParameter(":UPDATOR", OracleType.VarChar,64) ,            
+                        new OracleParameter(":UPDATETIME", OracleType.DateTime) ,            
+                        new OracleParameter(":ENDDATE", OracleType.DateTime) ,            
+                        new OracleParameter(":ACTNAME", OracleType.VarChar,1024) ,            
+                        new OracleParameter(":FILENUMBER", OracleType.VarChar,4000) ,            
+                        new OracleParameter(":PUBDATE", OracleType.DateTime) ,            
+                        new OracleParameter(":STADATE", OracleType.DateTime) ,            
+                        new OracleParameter(":DEPTS", OracleType.VarChar,1024) ,            
+                        new OracleParameter(":CONTENT", OracleType.Clob,4000) ,            
+                        new OracleParameter(":CREATEDTIME", OracleType.DateTime) ,            
+                        new OracleParameter(":EFFECT", OracleType.Char,2)             
+              
+            };
+
+                parameters[0].Value = model.ACTID;
+                parameters[1].Value = model.ID;
+                parameters[2].Value = model.ACTCODE;
+                parameters[3].Value = model.DEPTIDS;
+                parameters[4].Value = model.SRCTYPE;
+                parameters[5].Value = model.STATUS;
+                parameters[6].Value = model.DELFLAG;
+                parameters[7].Value = model.CREATOR;
+                parameters[8].Value = model.UPDATOR;
+                parameters[9].Value = model.UPDATETIME;
+                parameters[10].Value = model.ENDDATE;
+                parameters[11].Value = model.ACTNAME;
+                parameters[12].Value = model.FILENUMBER;
+                parameters[13].Value = model.PUBDATE;
+                parameters[14].Value = model.STADATE;
+                parameters[15].Value = model.DEPTS;
+                parameters[16].Value = model.CONTENT;
+                parameters[17].Value = model.CREATEDTIME;
+                parameters[18].Value = model.EFFECT;
+                DbHelperOra.ExecuteSql(strSql.ToString(), parameters);
+
+            }
+
+
+    
+
+            /// <summary>
+            /// 获得数据列表
+            /// </summary>
+            public DataSet GetList(string strWhere)
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.Append("select * ");
+                strSql.Append(" FROM T_CP_ACT ");
+                if (strWhere.Trim() != "")
+                {
+                    strSql.Append(" where " + strWhere);
+                }
+                return DbHelperOra.Query(strSql.ToString());
+            }
+
+            /// <summary>
+            /// 获得前几行数据
+            /// </summary>
+            public DataSet GetList(int Top, string strWhere, string filedOrder)
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.Append("select ");
+                if (Top > 0)
+                {
+                    strSql.Append(" top " + Top.ToString());
+                }
+                strSql.Append(" * ");
+                strSql.Append(" FROM T_CP_ACT ");
+                if (strWhere.Trim() != "")
+                {
+                    strSql.Append(" where " + strWhere);
+                }
+                strSql.Append(" order by " + filedOrder);
+                return DbHelperOra.Query(strSql.ToString());
+            }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.OracleClient;
+namespace XmlToOracleData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBHelper.init();
+            root r = new root();
+            r.act=new act() { actid = "43", content = "2", fileNumber = "df"};
+            
+            string a = SerializationHelper.SerializeObject<root>(r);
+            string basePath = @"E:\SkyDrive\Projects\浦发事项\上线程序\POC自动打包上线\bin\Debug\Data\201308211916";
+            string[] strFiles = Directory.GetFiles(basePath);
+            foreach (var item in strFiles)
+            {
+                StreamReader sr = new StreamReader(item);
+                string parm = sr.ReadToEnd();
+                sr.Close();
+                root roots = SerializationHelper.DeserializeObject<root>(parm);
+                DBInsert(roots);
+            }
+        }
+        static void DBInsert(root parm)
+        {
+              ActInsert(parm.act);
+              ActItemInsert(parm.act.actitems);
+          //  ActCorrelationInsert(parm.act.actcorrelations);
+        }
+        static void ActInsert(act parm)
+        {
+//            string sql = @"insert into T_CP_ACT (ID,ACTID,ACTNAME,FILENUMBER,CONTENT,PUBDATE,STADATE,DEPTS,CREATEDTIME,EFFECT,ACTCODE,DEPTIDS,SRCTYPE,STATUS,DELFLAG,UPDATETIME,ENDDATE)
+//                                        values (:ID,:ACTID,:ACTNAME,:FILENUMBER,:CONTENT,:PUBDATE,:STADATE,:DEPTS,:CREATEDTIME,:EFFECT,:ACTCODE,:DEPTIDS,:SRCTYPE,:STATUS,:DELFLAG,:UPDATETIME,:ENDDATE)";
+//            //:ID,:ACTID,:ACTNAME,:FILENUMBER,:CONTENT,:PUBDATE,:STADATE,:DEPTS,:CREATEDTIME,:EFFECT,:ACTCODE,:DEPTIDS,:SRCTYPE,:STATUS,:DELFLAG,:UPDATETIME,:ENDDATE
+//            OracleParameter[] parms ={
+//                                   new OracleParameter(":ID",OracleType.Number,32,parm.actid),
+//                                   new OracleParameter(":ACTID",OracleType.Number,32,parm.actid),
+//                                   new OracleParameter(":ACTNAME",parm.actname),
+//                                   new OracleParameter(":FILENUMBER",parm.fileNumber),
+//                                   new OracleParameter(":CONTENT",parm.content),
+//                                   new OracleParameter(":PUBDATE", Convert.ToDateTime(parm.pubdate)),//有问题
+//                                   new OracleParameter(":STADATE",Convert.ToDateTime(parm.stadate)),//有问题
+//                                   new OracleParameter(":DEPTS",parm.depts),
+//                                   new OracleParameter(":CREATEDTIME",DateTime.Now),
+//                                   new OracleParameter(":EFFECT",parm.effect),
+//                                   new OracleParameter(":ACTCODE",""),
+//                                   new OracleParameter(":DEPTIDS",parm.deptids),
+//                                   new OracleParameter(":SRCTYPE","1"),
+//                                   new OracleParameter(":STATUS","1"),
+//                                   new OracleParameter(":DELFLAG","N"),//如果是"Y"呢？
+//                                   new OracleParameter(":UPDATETIME",DateTime.Now),
+//                                   new OracleParameter(":ENDDATE",parm.enddate==null?DateTime.MinValue:Convert.ToDateTime(parm.enddate))
+//                                    };
+//            DBHelper.ExecuteNonQuery(sql, System.Data.CommandType.Text, parms);
+
+          //  string sql = "INSERT INTO T_CP_ACT (ID,ACTID,ACTNAME,FILENUMBER,CONTENT,PUBDATE,STADATE,DEPTS,CREATEDTIME,EFFECT,ACTCODE,DEPTIDS,SRCTYPE,STATUS,DELFLAG,UPDATETIME,ENDDATE) VALUES (" + parm.actid + "," + parm.actid + ",'" + parm.actname + "','" + parm.fileNumber + "','" + parm.content + "','" + parm.pubdate + "','" + parm.stadate + "','" + parm.depts + "','" + DateTime.Now.ToString("yyyy/MM/dd") + "'," + parm.effect + ",1,'" + parm.deptids + "',1,1,'N','" + DateTime.Now.ToString("yyyy/MM/dd") + "','"+parm.enddate+"')";
+          //  DBHelper.ExecuteNonQuery(sql);
+            T_CP_ACT act = new T_CP_ACT() { 
+             ID = Convert.ToInt32( parm.actid),
+              ACTCODE = "",
+               ACTID = Convert.ToInt32(parm.actid),
+                ACTNAME = parm.actname,
+                 CONTENT = parm.content,
+                  CREATEDTIME = DateTime.Now,
+                   CREATOR = "",
+                    DELFLAG = "N",
+                     DEPTIDS = parm.deptids,
+                      DEPTS = parm.depts,
+                       EFFECT = parm.effect,
+                         FILENUMBER = parm.fileNumber,
+                           SRCTYPE = "1",
+                             STATUS  = "1",
+                              UPDATETIME = DateTime.Now,
+                               UPDATOR = "",
+                                STADATE = DateTime.MinValue,
+                                 PUBDATE = DateTime.MinValue,
+                                  ENDDATE = DateTime.MinValue
+            };
+            if (parm.pubdate!=null&&parm.pubdate!=string.Empty)
+            {
+                act.PUBDATE =Convert.ToDateTime( parm.pubdate);
+            }
+            
+            if (parm.stadate != null && parm.stadate != string.Empty)
+            {
+                act.STADATE = Convert.ToDateTime(parm.stadate);
+            }
+            if (parm.enddate != null && parm.enddate != string.Empty)
+            {
+                act.ENDDATE = Convert.ToDateTime(parm.enddate);
+            }
+            act.Add(act);
+            Console.WriteLine("Act Inserted "+act.ACTID);
+        }
+        static void ActItemInsert(List<item> parm)
+        {
+            foreach (var model in parm)
+            {
+                StringBuilder strSql = new StringBuilder();
+                strSql.Append("insert into T_CP_ACT_ITEMS(");
+                strSql.Append("ITEMID,ACTID,ITEMNAME,ITEMTYPE,ITEMPARENTID,ORDERS,CONTENT");
+                strSql.Append(") values (");
+                strSql.Append(":ITEMID,:ACTID,:ITEMNAME,:ITEMTYPE,:ITEMPARENTID,:ORDERS,:CONTENT");
+                strSql.Append(") ");
+
+                OracleParameter[] parameters = {
+			            new OracleParameter(":ITEMID",  OracleType .Number,32) ,            
+                        new OracleParameter(":ACTID", OracleType.Number,32) ,            
+                        new OracleParameter(":ITEMNAME", OracleType.VarChar,512) ,            
+                        new OracleParameter(":ITEMTYPE", OracleType.Char,4) ,            
+                        new OracleParameter(":ITEMPARENTID", OracleType.Number,32) ,            
+                        new OracleParameter(":ORDERS", OracleType.Number,8) ,            
+                        new OracleParameter(":CONTENT", OracleType.Clob,4000)             
+            };
+
+                parameters[0].Value = model.itemid;
+                parameters[1].Value = model.actid;
+                parameters[2].Value = model.itemname;
+                parameters[3].Value = model.itemtype;
+                parameters[4].Value = model.itemparentid;
+                parameters[5].Value = model.orders;
+                parameters[6].Value = model.content;
+                DBHelper.ExecuteNonQuery(strSql.ToString(), System.Data.CommandType.Text, parameters);
+                Console.WriteLine("ActItem Inserted "+model.itemid);
+            }
+        }
+        static void ActCorrelationInsert(List<actcorrelation> parm)
+        {
+            foreach (var item in parm)
+            {
+                string sql = "insert into T_CP_ACT_CORRELATION (ID,TOACTID,TOITEMID,FROMACTID,FROMITEMID,FROMACTNAME,FROMITEMCONTENT,FROMITEMNAME) VALUES (:ID,:TOACTID,:TOITEMID,:FROMACTID,:FROMITEMID,:FROMACTNAME,:FROMITEMCONTENT,:FROMITEMNAME)";
+                //:TOACTID,:TOITEMID,:FROMACTID,:FROMITEMID,:FROMACTNAME,:FROMITEMCONTENT,:FROMITEMNAME
+                OracleParameter[] parms = { 
+                                              new OracleParameter(":ID",item.toitemid),
+                                            new OracleParameter(":TOACTID",item.toactid),
+                                            new OracleParameter(":TOITEMID",item.toitemid),
+                                            new OracleParameter(":FROMACTID",item.fromactid),
+                                            new OracleParameter(":FROMITEMID",item.fromitemid),
+                                            new OracleParameter(":FROMACTNAME",item.fromactname),
+                                            new OracleParameter(":FROMITEMCONTENT",item.fromitemcontent),
+                                            new OracleParameter(":FROMITEMNAME",item.fromitemname)
+                                          };
+                DBHelper.ExecuteNonQuery(sql,System.Data.CommandType.Text,parms);
+            }
+        }
+    }
+}
